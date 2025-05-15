5ab5e0b126aac55a43fa5da36e456d9d28d20c9b
simoneGuerra0 <148072510+simoneGuerra0@users.noreply.github.com>
2025-05-15 10:20:31 +0200

    correzione

--- A-Geometric-drawing-program_Gruppo05-AH/src/main/java/com/geometricdrawing/model/AbstractShape.java
+++ A-Geometric-drawing-program_Gruppo05-AH/src/main/java/com/geometricdrawing/model/AbstractShape.java
@@ -1,15 +1,20 @@
 package com.geometricdrawing.model;
 
+import javafx.scene.paint.Color;
+
 public abstract class AbstractShape implements Shape {
     // ... (campi x, y, width, height) ...
     protected Color borderColor = Color.BLACK; // Default per US-3
     protected Color fillColor = Color.TRANSPARENT; // Default per US-3 (o un colore solido per forme chiuse)
 
+    /*
     public AbstractShape(double x, double y, double width, double height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
     }
-    // ... resto come prima ...
+
+     */
+
 }
