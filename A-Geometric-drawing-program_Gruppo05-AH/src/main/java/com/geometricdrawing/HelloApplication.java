e2cbb0a7728a3a82afd9ae80600f0aa10f1ff8aa
simoneGuerra0 <148072510+simoneGuerra0@users.noreply.github.com>
2025-05-15 11:59:07 +0200

    Classi figure geometriche

--- A-Geometric-drawing-program_Gruppo05-AH/src/main/java/com/geometricdrawing/HelloApplication.java
+++ A-Geometric-drawing-program_Gruppo05-AH/src/main/java/com/geometricdrawing/HelloApplication.java
@@ -1,11 +1,8 @@
 package com.geometricdrawing;
 
-import com.geometricdrawing.model.DrawingModel;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,14 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-
-        BorderPane root = fxmlLoader.load();
-
-        DrawingController controller = fxmlLoader.getController();
-        DrawingModel model = new DrawingModel();
-        controller.setModel(model);
-
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
 
         stage.setScene(scene);
